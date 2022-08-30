336d149fa125aa827cf1fda21aa3641cfa41ef79
Ramesh c <ramesh_c@hotmail.com>
2022-08-30 18:17:22 +0000

    testing tf

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-=======
 # If you are in a workshop...
 # Do not delete this file!
 # It's required to complete the Instruqt labs.
 
->>>>>>> e08d6291e5d2dbe2785163b74f3bcabe3ed7a28d
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
@@ -13,8 +10,4 @@ terraform {
       name = "hashicat-aws"
     }
   }
-<<<<<<< HEAD
-}
-=======
-}
->>>>>>> e08d6291e5d2dbe2785163b74f3bcabe3ed7a28d
+
